e4f276e77d50fc77a586f0f2b6a4ceb60bee702f
Yaroslaw Biloshytskyi <yaroslaw0upwork@gmail.com>
2023-07-02 17:40:35 +0300

    Added search count to lualine

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -2,4 +2,12 @@ require("lualine").setup({
     options = {
         theme = "ayu_dark",
     },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "searchcount", "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    },
 })
